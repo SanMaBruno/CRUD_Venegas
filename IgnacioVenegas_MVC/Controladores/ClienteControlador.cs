020e14da89cdb6b449f3fbdfd1f0deb217a5ba28
Bruno San Martín <brunorodolfosanmartinnavarro@gmail.com>
2023-06-09 15:02:57 -0400

    CRUD_Venegas

---
+++ IgnacioVenegas_MVC/Controladores/ClienteControlador.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IgnacioVenegas_MVC.Controladores
+{
+    public class ClienteControlador
+    {
+        private ClienteDatos clienteDatos = new ClienteDatos();
+
+        public List<Cliente> ObtenerClientes()
+        {
+            return clienteDatos.ObtenerClientes();
+        }
+
+        public void AgregarCliente(Cliente cliente)
+        {
+            clienteDatos.AgregarCliente(cliente);
+        }
+
+        public void EliminarCliente(int id)
+        {
+            clienteDatos.EliminarCliente(id);
+        }
+    }
+
+}
---
+++ IgnacioVenegas_MVC/Modelos/Producto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IgnacioVenegas_MVC.Modelos
+{
+    class Producto
+    {
+        public int ID { get; set; }
+        public int Numero { get; set; }
+        public string Nombre { get; set; }
+    }
+}
